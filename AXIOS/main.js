fb9a2a0f1355b58b4c11c9b3e02a1e33938f18ca
Andreaboldrin97 <boldrinandrea23@gmail.com>
2022-08-25 17:25:12 +0200

    if for change call axios

--- AXIOS/main.js
+++ AXIOS/main.js
@@ -2,16 +2,22 @@ const app = new Vue ({
     el : '#app',
     data : {
         albums : [],
-        selectArtist : '',
+        selectGenre : '',
 
     },
     methods : {
         getAlbum : function(){
-            axios.get('http://localhost:8888/ESERCIZI%20PHP/php-ajax-dischi/AXIOS/controller.php')
+            let apiUrl = 'http://localhost:8888/ESERCIZI%20PHP/php-ajax-dischi/AXIOS/controller.php'
+
+            if(this.selectGenre != 'genre'){
+              apiUrl += '?'
+            }
+            axios.get(apiUrl)
             .then((element) => {
                 this.albums= element.data
                 console.log(this.albums)
             })
+           
         }
     },
     created(){
